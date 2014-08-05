e78db8e7a276a182440487b966431f69c9db66bb
brianherman <brianherman@gmail.com>
2014-08-05 08:04:02 -0500

    fixed app.py

--- app.py
+++ app.py
@@ -41,41 +41,22 @@ def allowed_file(filename):
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-<<<<<<< HEAD
-=======
     file = request.files['file']
->>>>>>> 44ab447a10c683fcea2d59a9bb60946080fbd3ca
     if request.method == 'POST':
         if file and allowed_file(file.filename):
-<<<<<<< HEAD
             os.mkdir(os.path.join(UPLOAD_PATH,str(session['token'])),777)
-            file.save(os.path.join(UPLOAD_PATH,str(session['token']),'upload.json'))
-#            parser.load(os.path.join(UPLOAD_PATH,str(session['token']),'upload.json'),\
-#                        os.path.join(UPLOAD_PATH,str(session['token']),'upload.kml'))
-#            splitter.load(os.path.join(UPLOAD_PATH,str(session['token'])+'upload.kml'),\
-#                          os.path.join(str(session['token'])))
-    return render_template("view.html")
-=======
-          if not os.path.exists(os.path.join(UPLOAD_PATH,str(session['token']))):
+            if not os.path.exists(os.path.join(UPLOAD_PATH,str(session['token']))):
               os.mkdir(os.path.join(UPLOAD_PATH,str(session['token'])))
               os.chmod(os.path.join(UPLOAD_PATH,str(session['token'])),0777)
-          file.save(os.path.join(UPLOAD_PATH,str(session['token']),'upload.json'))
-          the_parser = Parse()
-          file_to_parse = os.path.join( UPLOAD_PATH,str(session['token']), 'upload.json' )
-          result        = os.path.join(UPLOAD_PATH,str(session['token']),'upload.kml')
-          user_dir   = os.path.join(UPLOAD_PATH,str(session['token']))
-          print file_to_parse
-          the_splitter = Splitter()
-          the_parser.load(file_to_parse,result)
-          splitter.load(result,user_dir)
-#            os.mkdir('/home/brianherman/kml/static/'+str(session['token']),777)
-#            file.save('/home/brianherman/kml/static/'+str(session['token'])+'/upload.json')
-#            parser.load('/home/brianherman/kml/static/'+str(session['token'])+'upload.json',\
-#                        '/home/brianherman/kml/static'/+str(session['token'])+'upload.kml')
-#            splitter.load('/home/brianherman/kml/static/'+str(session['token'])+'upload.kml',\
-#                          '/home/brianherman/kml/static/'+str(session['token']))
-        return render_template("view.html",token=session['token'], number=10)
->>>>>>> 44ab447a10c683fcea2d59a9bb60946080fbd3ca
+        file.save(os.path.join(UPLOAD_PATH,str(session['token']),'upload.json'))
+        the_parser = Parse()
+        file_to_parse = os.path.join( UPLOAD_PATH,str(session['token']), 'upload.json' )
+        result        = os.path.join(UPLOAD_PATH,str(session['token']),'upload.kml')
+        user_dir   = os.path.join(UPLOAD_PATH,str(session['token']))
+        print file_to_parse
+        the_splitter = Splitter()
+        the_parser.load(file_to_parse,result)
+        splitter.load(result,user_dir)
 
 @app.route('/viewme/<number>')
 def viewme(number=None):
@@ -98,5 +79,6 @@ with open('secret_key.txt', 'r') as key_file:
     app.secret_key = secret_key 
 
 if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=5001)
 #    app.run(host='0.0.0.0', debug=True, port=5000)
 #    app.run(host='0.0.0.0', debug=True, port=80)
